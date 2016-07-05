67151782f065822873eab05ed389371255323be9
Martin Boonk <martin@boonk.info>
2016-07-05 21:36:43 +0200

    Initial work on shorewall install

--- debian/jessie/firewall/install.sh
+++ debian/jessie/firewall/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+. ../../../helper/helpers.sh
+
 apt-get --assume-yes install shorewall
-cp /usr/share/doc/shorewall/examples/one-interface/interfaces /etc/shorewall/
-cp /usr/share/doc/shorewall/examples/one-interface/policy /etc/shorewall/
-cp /usr/share/doc/shorewall/examples/one-interface/rules /etc/shorewall/
-cp /usr/share/doc/shorewall/examples/one-interface/zones /etc/shorewall/
+
+mkdir -p /etc/shorewall/rules.d
+replaceWithLink files/etc/shorewall/interfaces /etc/shorewall/interfaces
+replaceWithLink files/etc/shorewall/policy /etc/shorewall/policy
+replaceWithLink files/etc/shorewall/rules /etc/shorewall/rules
+replaceWithLink files/etc/shorewall/zones /etc/shorewall/zones
+replaceWithLink files/etc/default/shorewall /etc/default/shorewall
